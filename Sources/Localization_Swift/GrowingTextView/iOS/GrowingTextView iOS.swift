94fe5e24d813fd16cfa63a280e022622ab1e2bc2
waleedsa465 <waleedsa465@gmail.com>
2026-05-23 12:32:01 +0500

    commit after working on adding growing height for ios

---
+++ Sources/Localization_Swift/GrowingTextView/iOS/GrowingTextView iOS.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+@IBDesignable
+@MainActor
+open class GrowingTextView: PlaceholderTextView, UITextViewDelegate {
+
+    @IBInspectable public var maxNumberOfLines: Int = 6 {
+        didSet { recalcHeight() }
+    }
+
+    @IBInspectable public var minimumHeightConstrait: CGFloat = 48.0
+
+    private var heightConstraint: NSLayoutConstraint?
+
+    // MARK: - Initial Setup
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        Task { @MainActor in commonInit() }
+    }
+
+    override public init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        commonInit()
+    }
+
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        isScrollEnabled = true
+        isEditable = true
+        delegate = self
+
+        if heightConstraint == nil {
+            heightConstraint = heightAnchor.constraint(equalToConstant: minimumHeightConstrait)
+            heightConstraint?.isActive = true
+        }
+    }
+
+    override open func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        Task { @MainActor in
+            commonInit()
+            recalcHeight()
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    // MARK: - UITextViewDelegate Method
+    open func textViewDidChange(_ textView: UITextView) {
+        recalcHeight()
+    }
+
+    // MARK: - Recalculation of Height
+    open func recalcHeight() {
+        guard let font = font else { return }
+        let textHeight = contentSize.height
+        let lineHeight = font.lineHeight + 4
+        let maxHeight = lineHeight * CGFloat(maxNumberOfLines)
+        let finalHeight = min(maxHeight, textHeight)
+        heightConstraint?.constant = max(finalHeight, lineHeight + 12)
+    }
+
+    // MARK: - Layout Handling
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        recalcHeight()
+    }
+}
+
+extension UIFont {
+    var lineHeight: CGFloat {
+        return ascender + abs(descender) + leading
+    }
+}
---
+++ Sources/Localization_Swift/GrowingTextView/iOS/UITextViewClass.swift
@@ -0,0 +1,75 @@
+import Foundation
+import UIKit
+
+@IBDesignable
+@MainActor
+open class PlaceholderTextView: UITextView {
+    
+    
+    private let placeholderLabel: UILabel = UILabel()
+    
+    @IBInspectable var placeholder: String? {
+        didSet {
+            placeholderLabel.text = placeholder
+            placeholderLabel.sizeToFit()
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor = UIColor.lightGray {
+        didSet {
+            placeholderLabel.textColor = placeholderColor
+        }
+    }
+    public var placeHolderFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .regular)
+    
+    open override var text: String! {
+        didSet {
+            textDidChange()
+        }
+    }
+    
+    open override var font: UIFont? {
+        didSet {
+            placeholderLabel.font = placeHolderFont
+        }
+    }
+    
+    open override var textAlignment: NSTextAlignment {
+        didSet {
+            placeholderLabel.textAlignment = textAlignment
+        }
+    }
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        Task{ @MainActor in
+            setupPlaceholder()
+        }
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        placeholderLabel.frame.origin = CGPoint(x: textContainerInset.left + 3, y: textContainerInset.top)
+        placeholderLabel.frame.size.width = frame.width - textContainerInset.left - textContainerInset.right
+    }
+    
+    func setupPlaceholder() {
+        addSubview(placeholderLabel)
+        placeholderLabel.textColor = placeholderColor
+        placeholderLabel.font = placeHolderFont
+        placeholderLabel.textAlignment = textAlignment
+        placeholderLabel.numberOfLines = 0
+        placeholderLabel.isUserInteractionEnabled = false
+        placeholderLabel.backgroundColor = .clear
+        textDidChange()
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    @objc private func textDidChange() {
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    @MainActor deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
