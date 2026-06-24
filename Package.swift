661d3b0964e5f94ed761aa214a05723ad974b771
waleedsa465 <waleedsa465@gmail.com>
2026-06-24 13:54:58 +0500

    update version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Localization_Swift",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v10_15)
     ],
     products: [
