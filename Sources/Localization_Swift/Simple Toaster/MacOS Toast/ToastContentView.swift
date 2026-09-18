4746de72fa2931f3286f5cd19bbe9de042b70385
waleedsa465 <waleedsa465@gmail.com>
2026-09-18 11:55:50 +0500

    macos toast bug fixing

--- Sources/Localization_Swift/Simple Toaster/MacOS Toast/ToastContentView.swift
+++ Sources/Localization_Swift/Simple Toaster/MacOS Toast/ToastContentView.swift
@@ -14,10 +14,14 @@ struct ToastView: View {
             Text(message)
                 .multilineTextAlignment(.leading)
                 .foregroundStyle(textColor)
+                .frame(width: 230, alignment: .leading)
+                .fixedSize(horizontal: false, vertical: true)
         } else {
             Text(message)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(textColor)
+                .frame(width: 230, alignment: .leading)
+                .fixedSize(horizontal: false, vertical: true)
         }
     }
 
